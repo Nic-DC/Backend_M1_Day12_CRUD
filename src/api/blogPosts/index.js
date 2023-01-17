6b1fab5b8add01501435fb60280d4a8c2ce0c472
Nic <nic.dan.costea@gmail.com>
2023-01-17 16:08:51 +0100

    Read - endpoints

--- src/api/blogPosts/index.js
+++ src/api/blogPosts/index.js
@@ -7,6 +7,7 @@ const { NotFound, BadRequest } = createHttpError;
 
 const blogPostsRouter = express.Router();
 
+// POST new blog post
 blogPostsRouter.post("/", async (req, res, next) => {
   try {
     const newBlogPost = new BlogPostsModel(req.body);
@@ -21,24 +22,64 @@ blogPostsRouter.post("/", async (req, res, next) => {
     next(error);
   }
 });
+
+// 2. GET all blog posts
 blogPostsRouter.get("/", async (req, res, next) => {
   try {
+    const blogPosts = await BlogPostsModel.find();
+    if (blogPosts.length > 0) {
+      res.send(blogPosts);
+    } else {
+      next(NotFound(`There are no blog posts in our archive`));
+    }
   } catch (error) {
     next(error);
   }
 });
+
+// 3. GET single blog post
 blogPostsRouter.get("/:id", async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const searchedBlogPost = await BlogPostsModel.findById(id);
+
+    if (searchedBlogPost) {
+      res.send(searchedBlogPost);
+    } else {
+      next(NotFound(`The blog post with id: ${id} is not in our archive`));
+    }
   } catch (error) {
     next(error);
   }
 });
+
+// 4. PUT
 blogPostsRouter.put("/:id", async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const updatedBlogPost = await BlogPostsModel.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+
+    // OR
+    // using the MANGOOSE DOCUMENT <object that has available different methods like save()> as a result of findById, findOne..
+    // const updatedBlogPost = await BlogPostsModel.findById(id)
+    // updatedBlogPost.content = "testing the alternate solution of making a put request PUT "
+    // await updatedBlogPost.save()
+
+    if (updatedBlogPost) {
+      res.send(updatedBlogPost);
+      //   res.send({
+      //     message: `The blog post with id: ${id} has been successfully updated as you can see below`,
+      //     updatedBlogPost: updatedBlogPost,
+      //   });
+    } else {
+      next(NotFound(`The blog post with id: ${id} is NOT in our archive`));
+    }
   } catch (error) {
     next(error);
   }
 });
+
+// 5. DELETE
 blogPostsRouter.delete("/:id", async (req, res, next) => {
   try {
   } catch (error) {
